b7fcff70fe11b6bc42921cb398e6874b9fd567ba
Ialberquilla <ivan.alberquilla@gmail.com>
2022-04-22 15:12:12 +0200

    strategy open

--- contracts/contracts/StrategyManager.sol
+++ contracts/contracts/StrategyManager.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 import {IAddressProvider} from "./interfaces/IAddressProvider.sol";
 import {IAddressProvider} from "./interfaces/IAddressProvider.sol";
 import {UserVault} from "./UserVault.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
 contract StrategyManager {
     IAddressProvider private addressProvider;
@@ -16,6 +17,13 @@ contract StrategyManager {
     mapping(address => User) private userVaults;
     address[] users;
 
+    event StrategyOpened(
+        address indexed user,
+        address indexed collateral,
+        uint256 amount,
+        address creditManager
+    );
+
     constructor(address _geabboxAddressProvider) {
         addressProvider = IAddressProvider(_geabboxAddressProvider);
     }
@@ -23,17 +31,34 @@ contract StrategyManager {
     function createStrategy(
         address _creditManager,
         uint256 _timeFrame,
-        uint256 _windowSize
+        uint256 _windowSize,
+        uint256 _amount,
+        address _asset,
+        address _univ2Adapter,
+        address _yearnVault,
+        uint256 _leverageFactor
     ) public {
-        UserVault userVault = new UserVault();
+        UserVault userVault = new UserVault(
+            _creditManager,
+            _univ2Adapter,
+            _asset,
+            _yearnVault,
+            _leverageFactor
+        );
+        address userVaultAddress = address(userVault);
         User memory user = User(
             msg.sender,
-            address(this),
+            userVaultAddress,
             _timeFrame,
             _windowSize
         );
 
+        IERC20Upgradeable token = ERC20Upgradeable(_asset);
+        token.transferFrom(msg.sender, userVaultAddress, _amount);
+        userVault.openStrategy(_amount);
         userVaults[msg.sender] = user;
+
+        emit StrategyOpened(msg.sender, _asset, _amount, _creditManager);
     }
 
     function rebalance(address _userVault, uint256 _direction) public {}
--- contracts/contracts/UserVault.sol
+++ contracts/contracts/UserVault.sol
@@ -1,4 +1,39 @@
 pragma solidity ^0.8.0;
 
+import {ICreditManager} from "./interfaces/ICreditManager.sol";
+import {IUniV2Adapter} from "./interfaces/IUniV2Adapter.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
-contract UserVault {}
+contract UserVault {
+    // 0: Started, 1: Risky, 2: stable
+    uint256 status = 0;
+    address asset;
+    address yearVault;
+    uint256 leverageFactor;
+    ICreditManager private creditManager;
+    IUniV2Adapter private uniV2Adapter;
+
+    constructor(
+        address _creditManagerAddress,
+        address _univ2Adapter,
+        address _asset,
+        address _yearnVault,
+        uint256 _leverageFactor
+    ) {
+        asset = _asset;
+        yearVault = _yearnVault;
+        leverageFactor = _leverageFactor;
+        creditManager = ICreditManager(_creditManagerAddress);
+        uniV2Adapter = IUniV2Adapter(_univ2Adapter);
+    }
+
+    function openStrategy(uint256 _amount) public {
+        ERC20Upgradeable(asset).approve(address(creditManager), _amount);
+        creditManager.openCreditAccount(
+            _amount,
+            address(this),
+            leverageFactor,
+            0
+        );
+    }
+}
