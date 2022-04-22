27a25ae65dce64d599326463e11cc1815a2ce613
Ialberquilla <ivan.alberquilla@gmail.com>
2022-04-22 14:50:35 +0200

    user vault

--- contracts/contracts/StrategyManager.sol
+++ contracts/contracts/StrategyManager.sol
@@ -1,16 +1,40 @@
 pragma solidity ^0.8.0;
 
+import {IAddressProvider} from "./interfaces/IAddressProvider.sol";
+import {IAddressProvider} from "./interfaces/IAddressProvider.sol";
+import {UserVault} from "./UserVault.sol";
+
 contract StrategyManager {
+    IAddressProvider private addressProvider;
+
     struct User {
         address addr;
         address userVault;
         uint256 timeFrame;
         uint256 windowSize;
     }
-    mapping(address => User) private userVault;
+    mapping(address => User) private userVaults;
     address[] users;
 
-    constructor() {}
+    constructor(address _geabboxAddressProvider) {
+        addressProvider = IAddressProvider(_geabboxAddressProvider);
+    }
+
+    function createStrategy(
+        address _creditManager,
+        uint256 _timeFrame,
+        uint256 _windowSize
+    ) public {
+        UserVault userVault = new UserVault();
+        User memory user = User(
+            msg.sender,
+            address(this),
+            _timeFrame,
+            _windowSize
+        );
+
+        userVaults[msg.sender] = user;
+    }
 
     function rebalance(address _userVault, uint256 _direction) public {}
 
@@ -23,7 +47,7 @@ contract StrategyManager {
             uint256
         )
     {
-        User memory user = userVault[_user];
+        User memory user = userVaults[_user];
 
         return (user.addr, user.userVault, user.timeFrame, user.windowSize);
     }
