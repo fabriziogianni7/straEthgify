c373140f17a9f426b62bf41a4118e17445341286
obasilakis <vasileios.georgopoulos@gmail.com>
2022-04-23 17:33:46 +0200

    Make cron rebalance

--- cron/index.js
+++ cron/index.js
@@ -1,14 +1,18 @@
 import axios from 'axios'
-import Web3 from 'web3'
+import { ethers } from "ethers";
 import { addressToAsset } from './config.js';
 import abiJson from './StrategyManager.json'
-const contractAddress = process.env.contractAddress || '0xDE4eC895872d6889d03Bb7F6a08a0Ff19D8FFb5D'
-const web3 = new Web3("http://localhost:8545");
-const strategyContract = new web3.eth.Contract(abiJson.abi, contractAddress)
-const users = await strategyContract.methods.getAllUsers().call()
-console.log(users)
-const userVaults = await Promise.all(users.map(async user => await strategyContract.methods.getUserVault(user).call()))
-console.log(userVaults)
+
+const contractAddress = process.env.contractAddress || '0x53B551F2d4eC25e566de1D174509df3F7baB6c51'
+const provider = new ethers.providers.JsonRpcProvider();
+const wallet = new ethers.Wallet('0xe9b0203362f1a7c35310a7f4e566f247a9f17b335447fe599dfa848c1f00e376')
+wallet.connect(provider)
+const signer = provider.getSigner();
+const strategyContract = new ethers.Contract(contractAddress, abiJson.abi, signer);
+const users = await strategyContract.getAllUsers()
+
+const userVaults = await Promise.all(users.map(async user => await strategyContract.getUserVault(user)))
+
 const ethPricesResponse = await axios.get('https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=usd&days=max&interval=seconds')
 const btcPricesResponse = await axios.get('https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=max&interval=seconds')
 const ethPrices = ethPricesResponse.data.prices.map(el => el[1])
@@ -30,11 +34,9 @@ const calculateAverage = (timeframe, size, asset) => {
     return { average: relevantPrices.reduce((acc, curr) => acc + curr) / size, lastPrice: relevantPrices.slice(-1) }
 }
 await Promise.all(userVaults.map(async user => {
-    console.log(user[4])
-    const userAddress = user[0]
     const userVaultAddress = user[1]
-    const timeframe = Number(user[2]) / 3600
-    const size = Number(user[3])
+    const timeframe = parseInt(user[2]._hex, 16) / 3600
+    const size = parseInt(user[3]._hex, 16)
     const asset = addressToAsset[user[4]]
     const { average, lastPrice } = calculateAverage(timeframe, size, asset)
     let direction
@@ -44,35 +46,6 @@ await Promise.all(userVaults.map(async user => {
         direction = 0
     }
 
-    const nonce = await web3.eth.getTransactionCount('0x058B7Db751733178FE08721E558825c33f9ff29B', 'latest');
-
-    const data = strategyContract.methods.rebalance(userVaultAddress, direction).encodeABI()
-    const gas =  await web3.eth.estimateGas({
-        to: contractAddress,
-        data
-    })
-    const transaction = {
-        from: '0x058B7Db751733178FE08721E558825c33f9ff29B',
-        'to': contractAddress,
-        'value': 100,
-        gas,
-        data
-    };
-    console.log({data})
-
-    console.log('priv', process.env.PRIVATE_KEY)
-    const signedTx = await web3.eth.accounts.signTransaction(transaction, `0xe9b0203362f1a7c35310a7f4e566f247a9f17b335447fe599dfa848c1f00e376`);
-    web3.eth.sendSignedTransaction(signedTx.rawTransaction, function(error, hash) {
-
-        if (!error) {
-
-            console.log("🎉 The hash of your transaction is: ", hash, "\n Check Alchemy's Mempool to view the status of your transaction!");
-
-        } else {
-
-            console.log("❗Something went wrong while submitting your transaction:", error)
-
-        }
-
-    });
+    const response = await strategyContract.rebalance(userVaultAddress, direction)
+    console.log(await response.wait())
 }))
