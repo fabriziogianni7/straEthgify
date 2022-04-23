5400c25a4cfd24a146f1e43a7decca87a8878109
Ialberquilla <ivan.alberquilla@gmail.com>
2022-04-23 18:21:24 +0200

    mock data

--- frontend/src/components/linechart-component/StraEthChart.tsx
+++ frontend/src/components/linechart-component/StraEthChart.tsx
@@ -1,39 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import './css/StraEthChart.css';
 import ZoomableLineChart from './zoomableLineChart';
-import {  } from './mock-data'
+import { benchmark } from './mock-data'
 import { Fetcher } from '../../api/Fetcher';
 
 function StraEthChart() {
 
-  const [benchmark, setBenchmark] = useState()
-  const [firstStrategy, setFirstStrategy] = useState()
-  const [thirdData, setthirdData] = useState()
+  // const [benchmark, setBenchmark] = useState()
+  // const [firstStrategy, setFirstStrategy] = useState()
+  // const [thirdData, setthirdData] = useState()
 
 
 
-  useEffect(() => {
-    Fetcher({
-      "asset": "bitcoin",
-      "startingBalance": 10000,
-      "period": 100,
-      "startingDate": "01/01/2015"
-    }).then((response) =>{
-      console.log(response)
-      setBenchmark(response.benchmark)
-      setFirstStrategy(response.benchmark)
-      setthirdData(response.benchmark)
-    })
-  }, []);
+  // useEffect(() => {
+  //   Fetcher({
+  //     "asset": "bitcoin",
+  //     "startingBalance": 10000,
+  //     "period": 100,
+  //     "startingDate": "01/01/2015"
+  //   }).then((response) =>{
+  //     setBenchmark(response.benchmark)
+  //     setFirstStrategy(response.benchmark)
+  //     setthirdData(response.benchmark)
+  //   })
+  // }, []);
 
 
 
   return <div className="chart-group">
     <ZoomableLineChart
       benchmark={benchmark}
-      firstStrategy={firstStrategy}
-      secondStrategy={thirdData}
-      thirdStrategy={thirdData}
+      firstStrategy={benchmark}
+      secondStrategy={benchmark}
+      thirdStrategy={benchmark}
     />
   </div>
 }
